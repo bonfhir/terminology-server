29049381c41a3c6c172859d10bcc1d3453a0bd42
Maxime 'biximilien' Gauthier <maxime@alleycorpnord.com>
2024-01-24 15:56:06 -0500

    feat: Ensure definitions are loaded only once

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,15 +21,23 @@ if [ -z "$(docker ps -q -f name=$NAME 2>/dev/null)" ]; then
 fi
 
 # Start the container
+echo "Starting container '$NAME' ..."
 docker start $NAME
 
-# TODO: wait for the container to become ready
-sleep 10
+# wait for the container to become ready
+echo "Waiting for container '$NAME' to become ready ..."
+# shellcheck disable=SC2091
+until $(curl --output /dev/null -X "GET" -H "accept: application/fhir+json" --silent --fail "$URL/metadata"); do
+    printf '.'
+    sleep 5
+done
 
 # import definitions
-docker exec -it $NAME scripts/upload-definitions.sh \
-    "$URL" \
-    "$VERSION"
+if [ ! -e "configs/DEFINITIONS" ]; then
+    docker exec -it $NAME scripts/upload-definitions.sh \
+        "$URL" \
+        "$VERSION" && touch configs/DEFINITIONS
+fi
 
 # import SNOMED CT
 if [ ! -e "configs/SNOMED" ]; then
