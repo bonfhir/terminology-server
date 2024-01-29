30a3af30c64965a0741367ed5ffd0598a40d26e9
Maxime Gauthier <maxime@alleycorpnord.com>
2024-01-29 17:02:22 -0500

    Bootstrap Script Interface (#10)
    * feat: Progress on bootstrapping scripts
    
    Current progress on terminology server bootstrapping script.
    
    * feat: Remove config file and add terminologies to start script
    
    * feat: Add check to not re-import terminologies
    
    * feat: Ensure definitions are loaded only once
    
    * stuff
    
    * experiment
    
    * feat: Bootstrap script interface
    
    Add bootstrap script to load terminologies.
    
    * requested changes

--- terminologies/import-icd10.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-hapi-fhir-cli upload-terminology \
-    -d data/icd10cm-tabular-2023.xml \
-    -v r4 \
-    -t "http://localhost:8080/fhir" \
-    -u "http://hl7.org/fhir/sid/icd-10-cm"
--- terminologies/import-loinc.sh
+++
@@ -1,4 +0,0 @@
-## using Loinc v 2.72,
-## breaking changes in v 2.73
-## MultiAxialHierarchy/ becomes ComponentHierarchyBySystem/
-hapi-fhir-cli upload-terminology -d data/Loinc_2.72.zip -v r4 -t "http://localhost:8080/fhir" -u "http://loinc.org"
